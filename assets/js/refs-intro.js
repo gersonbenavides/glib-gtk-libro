a5684359a8cba9581da0026086de98638a84e9c5
Gerson Benavides <gersonbdev@gmail.com>
2021-10-09 02:12:27 -0500

    Page print references fix

---
+++ assets/js/refs-intro.js
@@ -0,0 +1,30 @@
+/* BEGIN REFERENCES INTRO */
+
+let i_01 = 0;
+
+let k_r_book = document.getElementsByClassName("k-r-book");
+for( i_01 = 0; i_01 < k_r_book.length; i_01++ ) {
+    k_r_book[i_01].innerHTML = "<a href=\"../00-intro/bibliography.html#k-r-book\">[1]</a>";
+}
+
+let oop_book = document.getElementsByClassName("oop-book");
+for( i_01 = 0; i_01 < oop_book.length; i_01++ ) {
+    oop_book[i_01].innerHTML = "<a href=\"../00-intro/bibliography.html#oop-book\">[2]</a>";
+}
+
+let algo_book = document.getElementsByClassName("algo-book");
+for( i_01 = 0; i_01 < algo_book.length; i_01++ ) {
+    algo_book[i_01].innerHTML = "<a href=\"../00-intro/bibliography.html#algo-book\">[4]</a>";
+}
+
+let unix_impatient = document.getElementsByClassName("unix-impatient");
+for( i_01 = 0; i_01 < unix_impatient.length; i_01++ ) {
+    unix_impatient[i_01].innerHTML = "<a href=\"../00-intro/bibliography.html#unix-impatient\">[5]</a>";
+}
+
+let pro_git = document.getElementsByClassName("pro-git");
+for( i_01 = 0; i_01 < pro_git.length; i_01++ ) {
+    pro_git[i_01].innerHTML = "<a href=\"../00-intro/bibliography.html#pro-git\">[6]</a>";
+}
+
+/* END REFERENCES INTRO */
